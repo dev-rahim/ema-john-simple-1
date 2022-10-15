12feb415e1e4b9c391e1e2709e8d77592ab64721
dev-rahim <rahimuddin5915@gmail.com>
2022-10-15 17:45:25 +0600

    product UI done

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Product.css'
 
-const Product = () => {
+const Product = (props) => {
+    console.log(props.product.name);
+    const { name, price, seller, stock, img } = props.product;
     return (
-        <div>
-            <h1>From Product</h1>
+        <div className='product-card'>
+            <img src={img} alt="" />
+            <div>
+                <h4>{name}</h4>
+                <p>by: {seller}</p>
+                <p>${price}</p>
+                <p>only {stock} left in stock - order soon</p>
+                <button className='add-to-cart-btn'>add to cart</button>
+            </div>
         </div>
     );
 };
--- src/Components/Store/Store.js
+++ src/Components/Store/Store.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Store.css'
 
 const Store = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch('./products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+        // console.log(products);
+    }, [])
     return (
-        <div className="Products">
-            <Product />
-            <Cart />
+        <div className="Store">
+            <div>
+                {
+                    products.map(pd => <Product key={pd.key} product={pd} />)
+                }
+            </div>
+            <div>
+                <Cart />
+            </div>
         </div>
     );
 };
